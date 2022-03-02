ab2c8b506e581f0cdc801faba9c89ed0cdf8e8b4
Carolina Arellano <97354399+carolinarellano@users.noreply.github.com>
2022-03-01 19:20:19 -0600

    Update Sandbox.py

--- Sandbox.py
+++ Sandbox.py
@@ -1,1 +1,22 @@
+#Used to practice and check the functions
+
+from Operators import and_function
+
+prueba = ['p', 'q', 'r', '(p→q)', '(p→q)∧r']
+
+proposition = {
+  'p': ['V', 'V', 'F', 'F'], 
+  'q': ['V', 'F', 'V', 'F']
+}
+
+def FuncionLuis(proposition):
+  return 'ArregloDividido'
+
+def funcionArlyn(funcionLuis):
+  return 'Dictionary'
+
+def FuncionCaro(funcionLuis, funcinoArlyn):
+  return 'TabladeVerdad'
+
+# print(and_function('p∧q', proposition['p'], proposition['q']))
 
